070033966b36c94d10554560db25698b58a422a2
pavanBingi <bingipavankumar63@gmail.com>
2024-11-22 12:08:01 +0530

    terms & conditions page Updated

---
+++ src/app/conditions/page.tsx
@@ -0,0 +1,23 @@
+// pages/aboutUs/page.tsx
+import Image from "next/image";
+
+export default function conditions() {
+  return (
+    <div className="relative w-full h-screen">
+      {/* Background Image */}
+      <Image
+        src="/background.jpg" // Update with your image's actual path
+        alt="Background"
+        fill
+        className="object-cover"
+      />
+
+      {/* Centered Title */}
+      <div className="absolute inset-0 flex items-center justify-center">
+        <h1 className="text-white text-5xl md:text-7xl font-bold drop-shadow-lg">
+        TERMS & CONDITIONS PAGE
+        </h1>
+      </div>
+    </div>
+  );
+}
